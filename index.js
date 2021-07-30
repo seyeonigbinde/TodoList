fe0d303e16f37816fb46963dd2e9bf908b3e84d9
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-07-30 02:15:49 +0100

    React App changed to Client

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ server.use(cors())
 
 const port = process.env.PORT
 
-server.use(express.static(path.join(__dirname, 'todolist-app/build')))
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.get('/*', (req, res) => {
   // if you want to serve a SPA using Express you totally can!
-  res.sendFile(path.join(__dirname, 'todolist-app/build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 server.get('/api', (req, res) => {
   res.json({ message: `${process.env.COHORT} To My Todo App` })
