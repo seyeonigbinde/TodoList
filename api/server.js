6816569f7c095661b6c734d773ba7b4298a8ffd2
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-07-30 13:42:05 +0100

    contentSecurityPolicy

--- api/server.js
+++ api/server.js
@@ -8,8 +8,13 @@ const authRouter = require('./auth/auth-router')
 const server = express()
 
 server.use(express.json())
-server.use(helmet())
+// server.use(helmet())
 server.use(cors())
+server.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 server.use('/api/auth', authRouter)
 server.use('/api', userRouter)
