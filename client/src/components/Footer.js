2cf83f89be436cca54653da3335565e738d7d83f
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-09-26 01:40:05 +0100

    App Footer Adjusted

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,22 +1,24 @@
-import React from "react"
+import React from 'react'
 
 const Footer = () => {
-
-
-    return (
-        <footer>
-            <section className="dash_footer" >
-                <div className="dash_copyright">
-                    <p>Copyright &copy; 2021 || Designed by <a href="https://seyeonigbinde.vercel.app/">Stevens Design Tech.</a></p>
-                </div>
-                <div className="dash_socials">
-                    <i class="fab fa-facebook-f fa-2x" style={{ color: '#fff' }}></i>
-                    <i class="fab fa-instagram fa-2x" style={{ color: '#fb3958' }}></i>
-                    <i class="fab fa-twitter fa-2x" style={{ color: 'skyblue' }}></i>
-                </div>
-            </section>
-        </footer>
-    )
+  return (
+    <footer>
+      <section className='dash_footer'>
+        <div className='dash_copyright'>
+          <p>
+            Copyright &copy; 2021 || Designed by{' '}
+            <a href='https://seyeonigbinde.vercel.app/'>Stevens Design Tech.</a>
+          </p>
+        </div>
+        <div className='dash_socials'>
+          <i class='fab fa-facebook-f' style={{ color: '#fff' }}></i>
+          <i class='fab fa-instagram' style={{ color: '#fb3958' }}></i>
+          <i class='fab fa-twitter' style={{ color: 'skyblue' }}></i>
+          <i class='fab fa-youtube' style={{ color: 'red' }}></i>
+        </div>
+      </section>
+    </footer>
+  )
 }
 
-export default Footer 
+export default Footer
